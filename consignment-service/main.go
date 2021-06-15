614abeb1c68265aeba01533689f9ec8b8ed63b77
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-15 11:06:55 +0800

    Server side codes

---
+++ consignment-service/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"context"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+	"log"
+	"net"
+	"sync"
+
+	pb "shippy/consignment-service/proto/consignment"
+)
+
+const (
+	PORT = ":50051"
+)
+
+type repository interface {
+	Create(consignment *pb.Consignment) (*pb.Consignment, error)
+}
+
+type Repository struct {
+	mu           sync.Mutex
+	consignments []*pb.Consignment
+}
+
+func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
+	repo.mu.Lock()
+	updated := append(repo.consignments, consignment)
+	repo.consignments = updated
+	repo.mu.Unlock()
+	return consignment, nil
+}
+
+type service struct {
+	repo repository
+}
+
+func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*pb.Response, error) {
+	consignment, err := s.repo.Create(req)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.Response{Created: true, Consignment: consignment}, nil
+}
+
+func main() {
+	repo := &Repository{}
+	lis, err := net.Listen("tcp", PORT)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterShippingServiceServer(s, &service{repo})
+	reflection.Register(s)
+	log.Println("Running on port:", PORT)
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
+}
