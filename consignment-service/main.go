c85187d3f3298d9064a1c44d5b8bec40044b9cee
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-16 18:04:43 +0800

    Delete unused comments

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -10,47 +10,6 @@ import (
 
 const defaultHost = "ksks.bokurano.live:27017"
 
-//type Repository struct {
-//	consignments []*pb.Consignment
-//}
-//
-//func (repo *Repository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-//	updated := append(repo.consignments, consignment)
-//	repo.consignments = updated
-//	return consignment, nil
-//}
-//
-//func (repo *Repository) GetAll() []*pb.Consignment {
-//	return repo.consignments
-//}
-//
-//type service struct {
-//	repo         Repository
-//	vesselClient vesselProto.VesselServiceClient
-//}
-//
-//func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, resp *pb.Response) error {
-//	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{MaxWeight: req.Weight, Capacity: int32(len(req.Containers))})
-//	log.Printf("Found vessel: %s \n", vesselResponse.Vessel.Name)
-//	if err != nil {
-//		return err
-//	}
-//	req.VesselId = vesselResponse.Vessel.Id
-//	consignment, err := s.repo.Create(req)
-//	if err != nil {
-//		return err
-//	}
-//	resp.Created = true
-//	resp.Consignment = consignment
-//	return nil
-//}
-//
-//func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, resp *pb.Response) error {
-//	consignments := s.repo.GetAll()
-//	resp.Consignments = consignments
-//	return nil
-//}
-
 func main() {
 	host := os.Getenv("DB_HOST")
 	if host == "" {
