279825c3c25fdc70f9bdac6edc9c4bd30690ef3d
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-18 17:52:45 +0800

    Token service

---
+++ user-service/token_service.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	pb "github.com/csh980717/shippy/user-service/proto/user"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+var key = []byte("lovenico_252")
+
+type CustomClaims struct {
+	User *pb.User
+	jwt.StandardClaims
+}
+
+type Authable interface {
+	Decode(token string) (*CustomClaims, error)
+	Encode(user *pb.User) (string, error)
+}
+
+type TokenService struct {
+	repo Repository
+}
+
+func (t *TokenService) Decode(tokenString string) (*CustomClaims, error) {
+	token, err := jwt.ParseWithClaims(tokenString, &CustomClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return key, nil
+	})
+	if claims, ok := token.Claims.(*CustomClaims); ok && token.Valid {
+		return claims, nil
+	} else {
+		return nil, err
+	}
+}
+
+func (t *TokenService) Encode(user *pb.User) (string, error) {
+	expireToken := time.Now().Add(time.Hour * 72).Unix()
+	claims := CustomClaims{
+		user, jwt.StandardClaims{
+			ExpiresAt: expireToken,
+			Issuer:    "user-service",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(key)
+}
