aa630de8291f566c94a387c1d1bc6249bc7da4a1
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-18 10:58:05 +0800

    User service handler

--- user-service/handler.go
+++ user-service/handler.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	pb "github.com/csh980717/shippy/user-service/proto/user"
+	"golang.org/x/net/context"
+)
+
+type service struct {
+	repo Repository
+}
+
+func (s service) Create(ctx context.Context, req *pb.User, res *pb.Response) error {
+	if err := s.repo.Create(req); err != nil {
+		return err
+	}
+	res.User = req
+	return nil
+}
+
+func (s service) Get(ctx context.Context, req *pb.User, res *pb.Response) error {
+	user, err := s.repo.Get(req.Id)
+	if err != nil {
+		return err
+	}
+	res.User = user
+	return nil
+}
+
+func (s service) GetAll(ctx context.Context, req *pb.Request, res *pb.Response) error {
+	users, err := s.repo.GetAll()
+	if err != nil {
+		return err
+	}
+	res.Users = users
+	return nil
+}
+
+func (s service) Auth(ctx context.Context, req *pb.User, res *pb.Token) error {
+	_, err := s.repo.GetByEmailAndPassword(req)
+	if err != nil {
+		return err
+	}
+	res.Token = "testingabc"
+	return nil
+}
+
+func (s service) ValidateToken(context context.Context, token *pb.Token, token2 *pb.Token) error {
+	return nil
+}
