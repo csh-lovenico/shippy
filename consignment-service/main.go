68d294b4ac3aec1564d28e0fa20ed427cd14ab80
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-19 17:20:00 +0800

    Validate token when using consignment service

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -1,9 +1,15 @@
 package main
 
 import (
+	"context"
+	"errors"
 	pb "github.com/csh980717/shippy/consignment-service/proto/consignment"
+	"github.com/csh980717/shippy/user-service/proto/user"
 	vesselProto "github.com/csh980717/shippy/vessel-service/proto/vessel"
 	"github.com/micro/go-micro"
+	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
+	"github.com/micro/go-micro/server"
 	"log"
 	"os"
 )
@@ -22,11 +28,30 @@ func main() {
 	}
 	s := micro.NewService(
 		micro.Name("consignment-service"),
-		micro.Version("latest"))
+		micro.Version("latest"),
+		micro.WrapHandler(AuthWrapper))
 	vesselClient := vesselProto.NewVesselServiceClient("vessel-service", s.Client())
 	s.Init()
 	pb.RegisterShippingServiceHandler(s.Server(), &service{session, vesselClient})
 	if err := s.Run(); err != nil {
 		log.Printf("failed to serve: %v", err)
+	}
+}
+
+func AuthWrapper(fn server.HandlerFunc) server.HandlerFunc {
+	return func(ctx context.Context, req server.Request, rsp interface{}) error {
+		meta, ok := metadata.FromContext(ctx)
+		if !ok {
+			return errors.New("no auth meta-data found in request")
+		}
+		token := meta["token"]
+		log.Println("Authenticating with token: ", token)
+		authClient := user.NewUserServiceClient("user-service", microclient.DefaultClient)
+		_, err := authClient.ValidateToken(context.Background(), &user.Token{Token: token})
+		if err != nil {
+			return err
+		}
+		err = fn(ctx, req, rsp)
+		return err
 	}
 }
