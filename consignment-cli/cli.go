a93120ca526a353c022647ffc5e714ae9c901efc
Shuhuai Cao <shuhuai.cao@outlook.com>
2021-06-19 17:19:12 +0800

    Pass token into consignment cli

--- consignment-cli/cli.go
+++ consignment-cli/cli.go
@@ -3,8 +3,10 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	pb "github.com/csh980717/shippy/consignment-service/proto/consignment"
 	microclient "github.com/micro/go-micro/client"
+	"github.com/micro/go-micro/metadata"
 	"github.com/micro/go-micro/v2/config/cmd"
 	"io/ioutil"
 	"log"
@@ -30,20 +32,26 @@ func main() {
 
 	client := pb.NewShippingServiceClient("consignment-service", microclient.DefaultClient)
 	file := defaultFilename
-	if len(os.Args) > 1 {
-		file = os.Args[1]
+	var token string
+	if len(os.Args) < 3 {
+		log.Fatal(errors.New("Not enough arguments, expecting file and token. "))
 	}
+	file = os.Args[1]
+	token = os.Args[2]
 	consignment, err := parseFile(file)
 	if err != nil {
 		log.Fatalf("Could not parse file: %v", err)
 	}
-	r, err := client.CreateConsignment(context.Background(), consignment)
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"token": token,
+	})
+	r, err := client.CreateConsignment(ctx, consignment)
 	if err != nil {
 		log.Fatalf("Could not greet: %v", err)
 	}
 	log.Printf("Created: %t", r.Created)
 
-	getAll, err := client.GetConsignments(context.Background(), &pb.GetRequest{})
+	getAll, err := client.GetConsignments(ctx, &pb.GetRequest{})
 	if err != nil {
 		log.Fatalf("Could not list consignments: %v", err)
 	}
